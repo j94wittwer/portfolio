3f94ae1d977d720bd8cbfe1fdb9cb64705cd1032
j94wittwer <j94wittwer@gmail.com>
2018-07-31 20:42:54 +0200

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,11 @@
+/*jshint esversion: 6 */
+
 // Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
 //Set initial State of menu
 let showMenu = false;
